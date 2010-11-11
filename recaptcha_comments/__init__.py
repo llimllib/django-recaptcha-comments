42c8f1304c7848f987b42950e32c7bbbc2c7e16d
bill <bill.mill@gmail.com>
2010-11-10 23:14:20 -0500

    starting to document

--- recaptcha_comments/__init__.py
+++ recaptcha_comments/__init__.py
@@ -2,5 +2,8 @@ from django.core import urlresolvers
 from django.contrib.comments.models import Comment
 from recaptcha_comments.forms import CommentCaptchaForm
 
+#Override the default django form with our CommentCaptchaForm. See 
+#the [django documentation](http://docs.djangoproject.com/en/dev/ref/contrib/comments/custom/)
+#for more details
 def get_form():
     return CommentCaptchaForm
