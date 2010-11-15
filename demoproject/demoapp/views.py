8bf96821894f25c0e98f933355afa8ab3a686672
bill <bill.mill@gmail.com>
2010-11-15 13:02:57 -0500

    completing the templatetag demo

--- demoproject/demoapp/views.py
+++ demoproject/demoapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response
-from demoapp.models import Homepage
 from django.template import RequestContext
+from django.conf import settings
+
+from demoapp.models import Homepage
+from recaptcha.client.captcha import submit as verify_captcha
 
 def homepage(request, **kwargs):
     #the object to attach the comments to
@@ -11,5 +14,19 @@ def homepage(request, **kwargs):
                               context_instance=RequestContext(request))
 
 def templatetag(request, **kwargs):
+    if request.POST:
+        comment = request.POST["comment"]
+        recaptcha_response_field = request.POST["recaptcha_response_field"]
+        recaptcha_challenge_field = request.POST["recaptcha_challenge_field"]
+        response = verify_captcha(recaptcha_challenge_field,
+                                  recaptcha_response_field,
+                                  settings.RECAPTCHA_PRIVATE_KEY,
+                                  request.META["REMOTE_ADDR"])
+        response = response.is_valid
+    else:
+        comment = None
+        response = None
+
     return render_to_response('templatetag.html',
+                              {"comment": comment, "response": response},
                               context_instance=RequestContext(request))
