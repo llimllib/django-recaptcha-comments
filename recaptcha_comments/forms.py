0a4597505540095109a87e071e54ffe4e6b99a14
bill <bill.mill@gmail.com>
2010-11-15 11:15:01 -0500

    better comments in forms.py

--- recaptcha_comments/forms.py
+++ recaptcha_comments/forms.py
@@ -1,18 +1,28 @@
-from django import forms
 from django.contrib.comments.forms import CommentForm
-from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 
 from recaptcha_comments.fields import ReCaptchaField
 
-COMMENT_MAX_LENGTH = getattr(settings,'COMMENT_MAX_LENGTH', 3000)
-
+# To customize the comment form, you can uncomment any or all of these
+# lines, move them into the CommentCaptchaForm class, and edit the
+# characteristics of the field you want to change. See
+# django's form fields documentation
+# [http://docs.djangoproject.com/en/dev/ref/forms/fields/](here)
+#
+# You may need some or all of these imports:
+# from django.conf import settings
+# from django import forms
+# from django.utils.translation import ungettext, ugettext_lazy as _
+#
+# and the comment field requires this line at the module level:
+# COMMENT_MAX_LENGTH = getattr(settings,'COMMENT_MAX_LENGTH', 3000)
+#
+#name          = forms.CharField(label=_("Name"), max_length=50)
+#email         = forms.EmailField(label=_("Email address"))
+#url           = forms.URLField(label=_("URL"),
+#                               required=False)
+#
+#comment       = forms.CharField(label=_('Comment'),
+#                                widget=forms.Textarea,
+#                                max_length=COMMENT_MAX_LENGTH)
 class CommentCaptchaForm(CommentForm):
-    name          = forms.CharField(label=_("What's your name?"), max_length=50)
-    email         = forms.EmailField(label=_("What's your email address?"))
-    url           = forms.URLField(label=_("What's your web site (optional)?"),
-                                   required=False)
-    comment       = forms.CharField(label=_('Your Comment (250 words or less)'),
-                                    widget=forms.Textarea,
-                                    max_length=COMMENT_MAX_LENGTH)
     captcha       = ReCaptchaField()
